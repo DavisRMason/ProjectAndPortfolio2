a887bd25404e162692400be70a33c23df834daee
BarackOBala <nbalaganesh@student.fullsail.edu>
2022-11-01 15:59:58 -0400

    Pause and Unpause function

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+    [Header("-----Player Stuff-----")]
+    public GameObject player;
+    public playerController playerScript;
+
+    [Header("-----UI-----")]
+    public GameObject pauseMenu;
+    public GameObject playerDeadMenu;
+    public GameObject winMenu;
+    public GameObject playerDamageScreen;
+    public TextMeshProUGUI enemiesLeft;
+
+    public int enemiesToKill;
+
+    public GameObject spawnPos;
+
+    public bool isPaused;
+
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+        spawnPos = GameObject.FindGameObjectWithTag("Spawn Pos");
+    }
+   
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && !playerDeadMenu.activeSelf && !winMenu.activeSelf)
+        {
+            isPaused = !isPaused;
+            pauseMenu.SetActive(isPaused);
+
+            if (isPaused)
+            {
+                pause();
+            }
+            else
+                unPause();
+        }
+    }
+    public void pause()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;  //cant leave the screen
+    }
+
+    public void unPause()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;  //locked in the centre of the screen
+    }
+    
+}
