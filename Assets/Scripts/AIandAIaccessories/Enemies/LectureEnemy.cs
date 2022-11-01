0ea4e5efc6498e97298280847f9edc8a59a0128a
Bryan-Santiago-Sinigaglia <leniel_25@hotmail.com>
2022-11-01 18:16:00 -0400

    Added temp LectureEnemy

---
+++ Assets/Scripts/AIandAIaccessories/Enemies/LectureEnemy.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LectureEnemy : MonoBehaviour, IDamage
+{
+    [Header("-----Components-----")]
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+
+    [Header("-----Enemy Stats-----")]
+    [SerializeField] int HP;
+    [SerializeField] int playerBaseSpeed;
+
+    [Header("-----Gun Stats-----")]
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform shootPos;
+    [SerializeField] float shootRate;
+
+    bool isShooting;
+    bool playerInRange;
+    Vector3 playerDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager.instance.enemiesToKill++;
+        gameManager.instance.updateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
+        playerDirection = (gameManager.instance.player.transform.position - transform.position);
+
+        if (playerInRange)
+        {
+            facePlayer();
+            if (!isShooting)
+                StartCoroutine(Shoot());
+        }
+    }
+
+    void facePlayer()
+    {
+        playerDirection.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(playerDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * playerBaseSpeed);
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+        StartCoroutine(FlashDamage());
+
+        if (HP <= 0)
+        {
+            gameManager.instance.updateEnemyNumber();
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator FlashDamage()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.3F);
+        model.material.color = Color.white;
+    }
+
+    IEnumerator Shoot()
+    {
+        isShooting = true;
+
+        Instantiate(bullet, shootPos.position, transform.rotation);
+
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = false;
+    }
+}
