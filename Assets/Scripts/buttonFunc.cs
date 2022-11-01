7e84badf6b7d9394a7b0d37e20294d411f998818
DavisRMason <dmason1@student.fullsail.edu>
2022-11-01 17:02:06 -0400

    button functionallity
    button func implemented

---
+++ Assets/Scripts/buttonFunc.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunc : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.unPause();
+        gameManager.instance.pauseMenu.SetActive(false);
+        gameManager.instance.isPaused = false;
+    }
+
+    public void restart()
+    {
+        gameManager.instance.unPause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        resume();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void respawn()
+    {
+        gameManager.instance.unPause();
+        //gameManager.instance.playerScript.playerRespawn();
+        //needs implemented in player control
+
+    }
+
+}
