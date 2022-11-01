76ad09aa642a612b18908cc743e725bfc8c94913
Bryan-Santiago-Sinigaglia <leniel_25@hotmail.com>
2022-11-01 17:41:44 -0400

    Middle of work need new Pull

---
+++ Assets/Scripts/AIandAIaccessories/Projectile.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Header("-----Components-----")]
+    [SerializeField] Rigidbody rb;
+
+    [Header("-----Bullet Stats-----")]
+    [SerializeField] int damage;
+    [SerializeField] int timer;
+    [SerializeField] int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, timer);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //gameManager.instance.playerScript.Damage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
