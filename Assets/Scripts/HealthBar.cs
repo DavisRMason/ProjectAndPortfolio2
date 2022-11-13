bcf66521ef895eb13978ab1af48ea0a89c88381a
DavisRMason <dmason1@student.fullsail.edu>
2022-11-13 18:37:38 -0500

    HP bar improved
    Health bar should now change colors as player loses health

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
-
+    public Gradient gradient;
+    public Image fill;
     public void SetMaxHealth(int HP)
     {
         slider.maxValue = HP;
         slider.value = HP;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int HP)
     {
         slider.value = HP;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
