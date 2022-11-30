645fd32ff9d50819032da1935be76ec85ba13b1e
Bryan-Santiago-Sinigaglia <leniel_25@hotmail.com>
2022-11-30 07:02:03 -0500

    Siren Enemy Base

---
+++ Assets/Scripts/AIandAIaccessories/Enemies/SirenEnemy.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class SirenEnemy : MonoBehaviour, IDamage
+{
+    [Header("-----Components-----")]
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject UI;
+    [SerializeField] Image HPBar;
+
+    [Header("-----Enemy Stats-----")]
+    [SerializeField] int HP;
+    [SerializeField] int playerBaseSpeed;
+    [SerializeField] int sightDist;
+    [SerializeField] int sightAngle;
+    [SerializeField] GameObject headPos;
+
+    [Header("-----Angle Flip-----")]
+    [SerializeField] int flipIt;
+
+    bool isDead;
+    bool playerInRange;
+    Vector3 playerDirection;
+    float angleToPlayer;
+    float stoppingDistOrig;
+    int hpOrig;
+
+
+    void Start()
+    {
+        hpOrig = HP;
+        stoppingDistOrig = agent.stoppingDistance;
+        UpdateHPBar();
+    }
+
+
+    void Update()
+    {
+        if (agent.enabled)
+        {
+            if (playerInRange)
+            {
+                CanSeePlayer();
+            }
+        }
+    }
+
+    void CanSeePlayer()
+    {
+        playerDirection = (gameManager.instance.player.transform.position - headPos.transform.position);
+
+        angleToPlayer = Vector3.Angle(playerDirection, transform.forward);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(headPos.transform.position, playerDirection, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= sightAngle)
+            {
+                agent.stoppingDistance = stoppingDistOrig;
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                FacePlayer();
+            }
+        }
+    }
+
+    void FacePlayer()
+    {
+        playerDirection.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(playerDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * playerBaseSpeed);
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+
+        UI.SetActive(true);
+        UpdateHPBar();
+        StartCoroutine(DisableUI());
+
+        agent.stoppingDistance = 0;
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
+        StartCoroutine(FlashDamage());
+
+        if (HP <= 0)
+        {
+            isDead = true;
+            gameManager.instance.updateEnemyNumber();
+            anim.SetBool("Dead", true);
+            agent.enabled = false;
+            UI.SetActive(false);
+            GetComponent<Collider>().enabled = false;
+            StartCoroutine(MegaDeath());
+        }
+    }
+
+    void UpdateHPBar()
+    {
+        HPBar.fillAmount = (float)HP / (float)hpOrig;
+    }
+
+    IEnumerator FlashDamage()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.3F);
+        model.material.color = Color.white;
+    }
+
+    IEnumerator DisableUI()
+    {
+        yield return new WaitForSeconds(7);
+        if (UI.activeInHierarchy)
+            UI.SetActive(false);
+    }
+
+    IEnumerator MegaDeath()
+    {
+        yield return new WaitForSeconds(60);
+        Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isDead)
+        {
+            playerInRange = true;
+            //gameManager.instance.player.transform.rotation.z += flipIt;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = false;
+    }
+}
