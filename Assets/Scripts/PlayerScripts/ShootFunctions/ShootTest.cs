d84b81a3f6e179edddd65834410b1b1bc94b6c8d
KaosArgoGames <redrevolver6c@gmail.com>
2022-11-30 11:18:23 -0500

    11-30-2022

--- Assets/Scripts/PlayerScripts/ShootFunctions/ShootTest.cs
+++ Assets/Scripts/PlayerScripts/ShootFunctions/ShootTest.cs
@@ -10,16 +10,12 @@ public class ShootTest : Shoot
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 2.0f;
-
-
-
         Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
 
-        float x = Mathf.Sin(Camera.main.transform.eulerAngles.x + 45 * Mathf.Deg2Rad);
-        float y = Mathf.Cos(Camera.main.transform.eulerAngles.y * Mathf.Deg2Rad);
-        float z = Mathf.Tan(Camera.main.transform.eulerAngles.z * Mathf.Deg2Rad);
-
-        Vector3 rot = new Vector3(x, y, z);
+        //float x = Mathf.Sin(Camera.main.transform.rotation.x * Mathf.Deg2Rad);
+        //float y = Mathf.Cos(Camera.main.transform.rotation.y * Mathf.Deg2Rad);
+        //float z = Mathf.Tan(Camera.main.transform.rotation.z * Mathf.Deg2Rad);
+        //Vector3 rot = new Vector3(x, y, z);
 
         Instantiate(obj, objectPos, Camera.main.transform.rotation);
         gameManager.instance.playerScript.aud.PlayOneShot(gameManager.instance.playerScript.shootAudioClip, gameManager.instance.playerScript.shootAudioVolume);
