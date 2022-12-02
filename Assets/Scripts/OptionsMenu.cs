a479a657b7df8efa444d78e162f42df65df27741
DavisRMason <dmason1@student.fullsail.edu>
2022-12-02 15:29:17 -0500

    Options Menu Saves Values
    working on saving the options in option menu

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
+
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer Mixer;
     public Camera cam;
 
+    private float volumeVal;
+    public Slider volumeslider;
+
+    private float fovVal;
+    public Slider fovSlider;
+
+    void Start()
+    {
+        volumeslider.value = PlayerPrefs.GetFloat("MainVolume");
+        fovSlider.value = PlayerPrefs.GetFloat("FOV");
+
+    }
+    void Update()
+    {
+        Mixer.SetFloat("MainVolume", volumeVal);
+        PlayerPrefs.SetFloat("MainVolume", volumeVal);
+
+        cam.fieldOfView = fovVal;
+        PlayerPrefs.SetFloat("FOV", fovVal);
+    }
+
     public void SetVolume(float vol)
     {
-        Mixer.SetFloat("MainVolume", vol);
+        volumeVal = vol;
     }
 
     public void SetFoV (float fov)
     {
-        cam.fieldOfView = fov;
+        fovVal = fov;
+    }
+    public float SetLookSens(float sens)
+    {
+        return sens;
     }
 }
