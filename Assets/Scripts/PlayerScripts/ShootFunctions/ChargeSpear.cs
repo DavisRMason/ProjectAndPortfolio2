9918c25838b1ff68e8fd685ea9eddb563ec36800
KaosArgoGames <redrevolver6c@gmail.com>
2022-12-02 14:07:52 -0500

    12-2-2022
    Fuckin Did it

--- Assets/Scripts/PlayerScripts/ShootFunctions/ChargeSpear.cs
+++ Assets/Scripts/PlayerScripts/ShootFunctions/ChargeSpear.cs
@@ -7,15 +7,5 @@ public class ChargeSpear : Shoot
     public override void shootBullet()
     {
         gameManager.instance.playerScript.spearMove = true;
-
-        RaycastHit hit;
-
-        if (Physics.SphereCast(gameManager.instance.playerScript.shootPoint.transform.position, 5, gameManager.instance.playerScript.shootPoint.transform.forward, out hit, 10))
-        {
-            if (hit.collider.GetComponent<IDamage>() != null)
-            {
-                hit.collider.GetComponent<IDamage>().takeDamage(gameManager.instance.playerScript.shootDamage);
-            }
-        }
     }
 }
