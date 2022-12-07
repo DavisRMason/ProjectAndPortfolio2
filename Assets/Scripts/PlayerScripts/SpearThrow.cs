f242796bc83c0278e91f8763fe46347e0ae2a6cb
KaosArgoGames <redrevolver6c@gmail.com>
2022-12-07 14:13:29 -0500

    12-7-2022

--- Assets/Scripts/PlayerScripts/SpearThrow.cs
+++ Assets/Scripts/PlayerScripts/SpearThrow.cs
@@ -59,7 +59,7 @@ public class SpearThrow : MonoBehaviour
                 Debug.DrawRay(attackPos.transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
                 hit.collider.GetComponent<IDamage>().takeDamage(gameManager.instance.playerScript.shootDamage);
                 Instantiate(gameManager.instance.playerScript.hitEffectTwo, attackPos.transform.position, gameObject.transform.rotation);
-                spearAudioSource.PlayOneShot(spearAudioClip, Random.Range(.5f, 1.0f) + audioLevel);
+                spearAudioSource.PlayOneShot(spearAudioClip);
             }
             else
             {
