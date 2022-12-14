b245123ada3030fc2157e4d45eab51383d50a0d6
DavisRMason <dmason1@student.fullsail.edu>
2022-12-14 18:09:48 -0500

    load screen added to hub doors
    load screen implemented and added to the doors in hub world

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChange : MonoBehaviour
 {
+
+    public GameObject LoadingScreen;
+    public Slider Slider;
+
     [SerializeField] string s;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(s);          
+            StartCoroutine(LoadAsync(s));
         }
 
+    }
+
+   IEnumerator LoadAsync (string s)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(s);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            Slider.value = progress;
+
+            yield return null;
+        }
     }
 }
